9fb9bd8acc7d54a76cd5c38cf9f367d9a5c4539b
DHARANYA A <dharanyaa.23it@kongu.edu>
2025-06-25 11:30:49 +0530

    Update egRouters.js

--- backend/routers/egRouters.js
+++ backend/routers/egRouters.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getRoute, putRoute, signupRoute, deleteRoute, getRouteById, loginRoute } = require('../controllers/egcontrollers.js')
+const { getRoute, putRoute, signupRoute, deleteRoute, getRouteById, loginRoute } = require('../controllers/egControllers.js')
 router.get('/get', getRoute)
 router.get('/get/:id', getRouteById)
 router.post('/signup',signupRoute)
